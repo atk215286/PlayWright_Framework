ab733fc3a31c5ebbe651516cbef4aa995fdd7153
atk215286 <atk215286@gmail.com>
2026-05-01 14:35:20 -0400

    may01

--- api-automation/src/test/java/com/api/tests/EndpointTest.java
+++ api-automation/src/test/java/com/api/tests/EndpointTest.java
@@ -5,8 +5,8 @@ import org.testng.annotations.Test;
 
 import com.api.endpoints.ClientEndpoint;
 
+import io.qameta.allure.Allure;
 import io.restassured.response.Response;
-
 public class EndpointTest {
     ClientEndpoint client = new ClientEndpoint();
 
@@ -27,6 +27,16 @@ public class EndpointTest {
     public void getObjectByIdTest() {
         Response response = client.getObjectById("1");
         response.then().statusCode(200);
+        System.out.println(response.getBody().asPrettyString());
+        System.out.println(" Device Name: " + response.jsonPath().getString("name"));
+        Allure.step("Test Result: " + response.getStatusCode());
+        Allure.step("Item Name: " + response.jsonPath().getString("name"));
+        Allure.step("Status Code: " + response.getStatusCode());
+        Allure.addAttachment("Response Body", "application/json", response.asPrettyString());
+        Allure.step("Name: " + response.jsonPath().getString("name"));
+
+       // System.out.println("Name of the Item; " + response.jsonPath().getList("name").toString());
+
     }
 
 }
